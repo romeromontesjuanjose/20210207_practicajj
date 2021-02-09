5951445e887b1c0980b4ffe34dcec13f1571bfee
romeromontesjuanjose <jjromero@iespuntadelverde.es>
2021-02-09 21:55:15 +0100

    first commit

--- src/consultas.js
+++ src/consultas.js
@@ -1,9 +1,34 @@
-db.ventas.aggregate([
-    {
-        $match: { 
-            $expr: { $gt: [{cantidad:"$detalle.cantidad"}, 3] },
+db.empleado.aggregate([
+    {$lookup:
+        {
+            from:"Departamento",
+            localField:"departamento",
+            foreignField:"id",
+            as:"númeroDepartamento"
         }
     },
+    {$project:
+        {
+            _id:0,
+            nombre_empleado:"$nombre",
+            nombre_departamento:{$arrayElemAt:["$númeroDepartamento.nombre",0]},
+            horas_trabajadas:"$horas",
+            sueldo_por_hora:{$arrayElemAt:["$númeroDepartamento.salarioPorHora",0]}
+        }
+    },
+    {$set:
+        {
+            sueldo_total:{$multiply:["$horas_trabajadas","$sueldo_por_hora"]}
+        }
+    },
+    {$sort:
+        {
+            nombre_departamento:1, nombre_empleado:1
+        }
+    }
+]).pretty()
+
+db.ventas.aggregate([
     {
         $group:{
             _id: { 
@@ -22,9 +47,18 @@ db.ventas.aggregate([
             Dia_compra: "$_id.dia",
             Obra_de_teatro: "$Obra_de_teatro",
             precio_total: "$venta_total",
-            precio_IVA: { $multiply: ["$venta_total", 0.21] },
-            precio_total_IVA: { $multiply: ["$venta_total", 1.21] },
-            precio_total_IVA_redondeado: { $round: [{ $multiply: ["$venta_total", 1.21] }, 0] }
+        }
+    },
+    {
+        $match: { 
+            $expr: { $gt: [{cantidad:"$detalle.cantidad"}, 3] },
+        }
+    },
+    {$set:
+        {
+            precio_IVA: { $multiply: ["$precio_total", 0.21] },
+            precio_total_IVA: { $multiply: ["$precio_total", 1.21] },
+            precio_total_IVA_redondeado: { $round: [{ $multiply: ["$precio_total", 1.21] }, 0] }
         }
     },
     {
